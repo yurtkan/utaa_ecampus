273779d12947c1a0c6d351e4001e3285bf66e30f
Yurtkan Yurtoğulları <12103132+yurtkan@users.noreply.github.com>
2023-05-13 22:33:34 +0300

    oriantation problem solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:utaa_ecampus/screens/home/home_screen.dart';
+import 'package:utaa_ecampus/screens/notfoundscreen/not_found_screen.dart';
 import 'package:utaa_ecampus/testpage.dart';
-//import '../screens/home.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'UTAA eCampus',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        // appBarTheme: const AppBarTheme(
-        //   systemOverlayStyle: SystemUiOverlayStyle(
-        //     statusBarColor: Colors.transparent,
-        //     systemStatusBarContrastEnforced: false,
-        //     systemNavigationBarColor: Colors.transparent,
-        //     systemNavigationBarContrastEnforced: false,
-        //   ),
-        // ),
+  //runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (value) => runApp(
+      GetMaterialApp(
+        title: 'UTAA eCampus',
+        debugShowCheckedModeBanner: false,
+        //home: TestPage(),
+        unknownRoute: GetPage(name: '/notfound', page: () => NotFoundScreen()),
+        initialRoute: '/test',
+        getPages: [
+          GetPage(
+            name: '/test',
+            page: () => TestPage(),
+          ),
+          GetPage(
+            name: '/home',
+            page: () => HomeScreen(),
+          ),
+        ],
       ),
-      debugShowCheckedModeBanner: false,
-      home: const TestPage(),
-      //home: SideMenu(),
-    );
-  }
+    ),
+  );
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'UTAA eCampus',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         // appBarTheme: const AppBarTheme(
+//         //   systemOverlayStyle: SystemUiOverlayStyle(
+//         //     statusBarColor: Colors.transparent,
+//         //     systemStatusBarContrastEnforced: false,
+//         //     systemNavigationBarColor: Colors.transparent,
+//         //     systemNavigationBarContrastEnforced: false,
+//         //   ),
+//         // ),
+//       ),
+//       debugShowCheckedModeBanner: false,
+//       home: const TestPage(),
+//       //home: SideMenu(),
+//     );
+//   }
+// }
