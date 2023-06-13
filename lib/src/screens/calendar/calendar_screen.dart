b039acb4ffb3001ea39c3616184c6991bd5ef766
Yurtkan Yurtoğulları <12103132+yurtkan@users.noreply.github.com>
2023-06-13 23:52:18 +0300

    a

--- lib/src/screens/calendar/calendar_screen.dart
+++ lib/src/screens/calendar/calendar_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:table_calendar/table_calendar.dart';
 import 'package:utaa_ecampus/src/controllers/calendar_controller.dart';
 
 class CalendarScreen extends StatelessWidget {
@@ -22,37 +21,7 @@ class CalendarScreen extends StatelessWidget {
               color: Colors.black,
             ),
           )),
-      body: Obx(
-        () => TableCalendar(
-          firstDay: calendarController.firstDay,
-          lastDay: calendarController.lastDay,
-          focusedDay: calendarController.focusedDay,
-          calendarFormat: calendarController.calendarFormat,
-          selectedDayPredicate: (day) {
-            // Use `selectedDayPredicate` to determine which day is currently selected.
-            // If this returns true, then `day` will be marked as selected.
-
-            // Using `isSameDay` is recommended to disregard
-            // the time-part of compared DateTime objects.
-            return isSameDay(calendarController.selectedDay, day);
-          },
-          onDaySelected: (selectedDay, focusedDay) {
-            if (!isSameDay(calendarController.selectedDay, selectedDay)) {
-              calendarController.selectedDay = selectedDay;
-              calendarController.focusedDay = focusedDay;
-            }
-          },
-          onFormatChanged: (format) {
-            if (calendarController.calendarFormat != format) {
-              calendarController.calendarFormat = format;
-            }
-          },
-          onPageChanged: (focusedDay) {
-            // No need to call `setState()` here
-            calendarController.focusedDay = focusedDay;
-          },
-        ),
-      ),
+      body: Placeholder(),
     );
   }
 }
